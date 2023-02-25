775a28bd0b84dc0a2e07b696980aae2a4586d974
hilmi2321 <firdaus22303@gmail.com>
2023-02-25 21:09:02 +0700

    Penambahan js dan gambar menjadi webp

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Selamat Datang");
+var lagi = true;
+
+while (lagi) {
+  var nama = prompt("Masukkan Nama: ");
+  alert("Halo  " + nama);
+  lagi = confirm("Coba lagi?");
+}
+
+alert("Terima Kasih");
